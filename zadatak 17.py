9816add8ea36cc47c6759c5bec6678427d796749
korisnik <milovan4438jovic@gmail.com>
2024-10-05 04:03:19 -0700

    poruka

---
+++ zadatak 17.py
@@ -0,0 +1,4 @@
+stranicaa=int(input("unesi stranicu a"))
+stranicab=int(input("unesi stranicu b"))
+print(" obim je ",2*stranicaa+2*stranicab)
+print(" površina ",stranicaa*stranicab)                                                                                                                                                
